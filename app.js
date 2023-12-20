8f2c7a9a157cf75543e91541c957d86ccc80ed2b
bnymcontactus <141715573+bnymcontactus@users.noreply.github.com>
2023-12-21 08:14:30 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
+
 document.addEventListener("DOMContentLoaded", function() {
     var canvas = new fabric.Canvas('c');
     var isGroupingMode = false;
-    var previousGroup = null;
 
     document.getElementById('toggleButton').addEventListener('click', function() {
         isGroupingMode = !isGroupingMode;
         canvas.isDrawingMode = !isGroupingMode;
     });
 
-    canvas.on('selection:created', function(e) {
-        if (isGroupingMode) {
-            var group = new fabric.Group(e.selected, { canvas: canvas });
-            canvas.add(group);
-            canvas.discardActiveObject();
-            if (previousGroup) {
-                var line = new fabric.Line(
-                    [previousGroup.left, previousGroup.top, group.left, group.top], 
-                    { stroke: 'black', selectable: false }
-                );
-                canvas.add(line);
-            }
-            previousGroup = group;
+    document.getElementById('groupButton').addEventListener('click', function() {
+        if (canvas.getActiveObject() && canvas.getActiveObject().type === 'activeSelection' && isGroupingMode) {
+            canvas.getActiveObject().toGroup();
+            canvas.requestRenderAll();
+        }
+    });
+
+    document.getElementById('ungroupButton').addEventListener('click', function() {
+        if (canvas.getActiveObject() && canvas.getActiveObject().type === 'group' && isGroupingMode) {
+            canvas.getActiveObject().toActiveSelection();
+            canvas.requestRenderAll();
         }
     });
 
