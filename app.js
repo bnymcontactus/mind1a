e8919731c31d5cbed872725f38e52edc11fd8c7e
bnymcontactus <141715573+bnymcontactus@users.noreply.github.com>
2023-12-22 10:50:44 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     var canvas = new fabric.Canvas('c');
     var isDrawingMode = true;
+    var watermarkText = "123"; // Replace with your desired watermark number
 
     canvas.isDrawingMode = isDrawingMode;
+
+    // Create a static, non-interactive watermark
+    var watermark = new fabric.Text(watermarkText, {
+        fontSize: 100,
+        fill: 'grey',
+        left: 50,
+        top: 50,
+        selectable: false,
+        evented: false
+    });
+    canvas.add(watermark);
+    canvas.sendToBack(watermark); // Send watermark to back
 
     document.getElementById('toggleButton').addEventListener('click', function() {
         isDrawingMode = !isDrawingMode;
@@ -10,13 +23,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
         if (!isDrawingMode) {
             var objectsToGroup = canvas.getObjects().filter(function(obj) {
-                // Only group objects that are not part of a group
-                return obj.type !== 'group';
+                return obj.type !== 'group' && obj !== watermark;
             });
 
             if (objectsToGroup.length > 0) {
                 var newGroup = new fabric.Group(objectsToGroup, { canvas: canvas });
-                // Remove the individual objects and add the group instead
                 objectsToGroup.forEach(function(obj) {
                     canvas.remove(obj);
                 });
