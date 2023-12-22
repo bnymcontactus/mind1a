89209b30be28e909bb7535eed794462c50449e42
bnymcontactus <141715573+bnymcontactus@users.noreply.github.com>
2023-12-22 11:36:18 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,80 @@
 document.addEventListener("DOMContentLoaded", function() {
     var canvas = new fabric.Canvas('c');
     var isDrawingMode = true;
-    var newGroup = null;
+    var currentGroup = null;
+    var previousGroup = null;
 
-    // Initialize the canvas for drawing
+    // Enable free drawing by default
     canvas.isDrawingMode = isDrawingMode;
 
-    // Toggle button event listener
-    document.getElementById('toggleButton').addEventListener('click', function() {
-        isDrawingMode = !isDrawingMode;
-        canvas.isDrawingMode = isDrawingMode;
-
-        if (!isDrawingMode && canvas.getActiveObject() && canvas.getActiveObject().type !== 'group') {
-            var objectsToGroup = canvas.getActiveObject().getObjects();
-            newGroup = new fabric.Group(objectsToGroup, { canvas: canvas });
-            objectsToGroup.forEach(obj => canvas.remove(obj));
-            canvas.add(newGroup);
-        }
+    // Create a watermark
+    var watermark = new fabric.Text('Watermark', {
+        fontSize: 100,
+        fill: 'grey',
+        left: 50,
+        top: 50,
+        selectable: false,
+        evented: false
     });
+    canvas.add(watermark);
+    canvas.sendToBack(watermark);
 
-    // Group selection event listener for drawing lines
-    canvas.on('selection:created', function(e) {
-        if (!isDrawingMode && newGroup && e.target && e.target.type === 'group' && e.target !== newGroup) {
-            drawLineBetweenGroups(newGroup, e.target);
-            newGroup = null; // Reset newGroup after drawing the line
-        }
-    });
-
-    function drawLineBetweenGroups(group1, group2) {
-        var lineCoords = [group1.left, group1.top, group2.left, group2.top];
-        var line = new fabric.Line(lineCoords, {
+    // Function to create a line
+    function createLine(fromGroup, toGroup) {
+        if (!fromGroup || !toGroup) return;
+        var points = [fromGroup.left, fromGroup.top, toGroup.left, toGroup.top];
+        var line = new fabric.Line(points, {
             fill: 'blue',
             stroke: 'blue',
             strokeWidth: 2,
             selectable: false,
-            evented: false
+            evented: false,
+            start: fromGroup,
+            end: toGroup
         });
+
         canvas.add(line);
-
-        // Add line update logic to group moving events
-        group1.on('moving', function() { updateLinePosition(line, group1, group2); });
-        group2.on('moving', function() { updateLinePosition(line, group1, group2); });
+        return line;
     }
 
-    function updateLinePosition(line, startObj, endObj) {
-        line.set({ 'x1': startObj.left, 'y1': startObj.top, 'x2': endObj.left, 'y2': endObj.top });
-        canvas.requestRenderAll();
+    // Update line position when groups move
+    function updateLine(line) {
+        var points = [line.start.left, line.start.top, line.end.left, line.end.top];
+        line.set({ x1: points[0], y1: points[1], x2: points[2], y2: points[3] });
+        canvas.renderAll();
     }
+
+    // Toggle drawing mode
+    document.getElementById('toggleButton').addEventListener('click', function() {
+        isDrawingMode = !isDrawingMode;
+        canvas.isDrawingMode = isDrawingMode;
+
+        if (!isDrawingMode) {
+            var objectsToGroup = canvas.getObjects().filter(function(obj) {
+                return obj.type !== 'group' && obj !== watermark;
+            });
+
+            if (objectsToGroup.length > 0) {
+                previousGroup = currentGroup;
+                currentGroup = new fabric.Group(objectsToGroup, { canvas: canvas });
+                objectsToGroup.forEach(function(obj) {
+                    canvas.remove(obj);
+                });
+                canvas.add(currentGroup);
+
+                if (previousGroup) {
+                    var line = createLine(previousGroup, currentGroup);
+                    currentGroup.line = line;
+                    previousGroup.line = line;
+
+                    currentGroup.on('moving', function() {
+                        updateLine(line);
+                    });
+                    previousGroup.on('moving', function() {
+                        updateLine(line);
+                    });
+                }
+            }
+        }
+    });
 });
